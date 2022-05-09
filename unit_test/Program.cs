424301618c9f5035682e7a37df08b6fce026de1c
cbrasseur <cdric.brasseur@gmail.com>
2022-05-09 09:17:15 +0200

    message

--- unit_test/Program.cs
+++ unit_test/Program.cs
@@ -7,6 +7,7 @@ namespace unit_test
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
